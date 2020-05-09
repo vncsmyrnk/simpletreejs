fa3f7e2f298f804b07ddaf3f2a8c973773a08f1d
Vinicius Mayrink <vmayrink12@gmail.com>
2020-05-09 18:24:12 -0300

    Adding click to open funcionality

--- tree.js
+++ tree.js
@@ -2,23 +2,18 @@ const menuItems = [
     { id: 1, name: 'Eletronicos', parent: null },
     { id: 2, name: 'Departamento', parent: null },
     { id: 3, name: 'Escritorio', parent: null },
-
     { id: 4, name: 'Computadores', parent: 1 },
     { id: 5, name: 'Celulares', parent: 1 },
     { id: 6, name: 'Eletrodomesticos', parent: 1 },
-
     { id: 7, name: 'Notebook', parent: 4 },
     { id: 8, name: 'PC', parent: 4 },
     { id: 9, name: 'Mac', parent: 4 },
-
     { id: 10, name: 'Mesa', parent: 3 },
-
     { id: 11, name: 'Roupas', parent: 2 },
     { id: 12, name: 'Camisas', parent: 11 },
     { id: 13, name: 'Acessorios', parent: 2 },
     { id: 14, name: 'Blusas', parent: 11 },
     { id: 15, name: 'Com manga', parent: 14 },
-
 ];
 
 class Menu {
@@ -29,60 +24,82 @@ class Menu {
     }
 
     buildMenu() {
-        if (this.idsDuplicados()) {
-            throw 'Ids duplicados;'
+        if (this.duplicatedIds()) {
+            throw 'Duplicated Ids';
         }
     
-        // lista
+        // Root list
         const ul = document.createElement('ul');
     
-        // elementos raizes
-        const raiz = this.menuItems.filter(n => n.parent == null);
+        // Root elements
+        const rootNodes = this.menuItems.filter(n => n.parent == null);
     
-        for (let node of raiz) {
-            let li = document.createElement('li');
-            li.innerHTML = node.name;
-            ul.appendChild(li);
-            this.montaGalhos(node, li);
+        for (let node of rootNodes) {
+            let nodeLi = document.createElement('li');
+            nodeLi.innerHTML = `<a>${node.name}</a>`;
+
+            ul.appendChild(nodeLi);
+            this.buildBranches(node, nodeLi);
         }
-    
-        // adiciona lista ao body
+        
+        // Add list to body so it can be renderized
         document.body.appendChild(ul);
     }
     
-    montaGalhos(node, nodeLi) {
-        // menu: lista, node: nodulo  do pai; nodeLi: li do nodulo pai
+    buildBranches(node, nodeLi, closed = true) {
+        // branches
+        const branches = this.menuItems.filter(n => n.parent == node.id);
     
-        // filhos
-        const galhos = this.menuItems.filter(n => n.parent == node.id);
-    
-        if (galhos) {
-            // ul dos galhos
+        if (branches.length > 0) {
+            // has branches, add css class
+            nodeLi.classList.add('item-parent');
+
+            // Add function to hide child nodes on mouse click
+            nodeLi.onclick = (event) => { 
+                event.stopPropagation();
+                this.toggleVisiblityChildNodes(nodeLi) 
+            }
+            
+            // Branches ul
             const ul = document.createElement('ul');
+
+            // Creation parameter
+            ul.style.display = closed ? 'none' : 'block';
         
-            for (let galho of galhos) {
+            for (let galho of branches) {
                 let li = document.createElement('li');
-                li.innerHTML = galho.name;
+                li.innerHTML = `<a>${galho.name}</a>`;
+                li.onclick = (event) => {
+                    event.stopPropagation();
+                }
+
                 ul.appendChild(li);
     
-                // insere galho
+                // Add branch
                 nodeLi.appendChild(ul)
         
-                this.montaGalhos(galho, li);
+                this.buildBranches(galho, li);
             }
         }
     }
     
-    idsDuplicados() {
+    duplicatedIds() {
         for (let menuItem of this.menuItems) {
-            let repeticao = this.menuItems.filter(n => n.id == menuItem.id);
+            const sameIdNodes = this.menuItems.filter(n => n.id == menuItem.id);
+            const nodeWithSameIdExists = sameIdNodes.length > 1;
     
-            if (repeticao.length > 1) {
+            if (nodeWithSameIdExists) {
                 return true;
             }
         }
     
         return false;
+    }
+
+    toggleVisiblityChildNodes(nodeLi) {
+        const internUl = nodeLi.childNodes[1];
+        internUl.style.display = internUl.style.display == 'none' ? 'block' : 'none';
+        nodeLi.classList.toggle('item-parent--open');
     }
 }
 
